4fc723a94d95aac4534d2438419cbd40c3eed866
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-12-01 23:51:52 +0900

    1

---
+++ ciif/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2021, 09, 14, 9)}); // year, month, date, hour
+        });
+});		
+
